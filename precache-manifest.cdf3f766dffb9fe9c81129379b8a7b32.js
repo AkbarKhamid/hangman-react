9e3dc693a7e4173b806a0f609abd84ebb27f6ade
KhamidovAkbar <thesh3yx@gmail.com>
2020-10-20 14:39:03 +0900

    Updates

---
+++ precache-manifest.cdf3f766dffb9fe9c81129379b8a7b32.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "aa34d434366afdd714d3dd183964967c",
+    "url": "/hangman-react/index.html"
+  },
+  {
+    "revision": "3c28b1fa2a98b1fb48a3",
+    "url": "/hangman-react/static/css/main.bbf2715b.chunk.css"
+  },
+  {
+    "revision": "ab17d89143385087f744",
+    "url": "/hangman-react/static/js/2.1f38aa13.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/hangman-react/static/js/2.1f38aa13.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3c28b1fa2a98b1fb48a3",
+    "url": "/hangman-react/static/js/main.7e82a6d7.chunk.js"
+  },
+  {
+    "revision": "3f7c69ac9d5e568f51ba",
+    "url": "/hangman-react/static/js/runtime-main.0e2bf36f.js"
+  }
+]);
